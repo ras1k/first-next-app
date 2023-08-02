0c2dc9bb93cac9120662ef28927d5e4b55579bbe
Rasik Abdullah <rasikabdullah@gmail.com>
2023-08-02 17:15:41 +0600

    first commit

---
+++ src/components/HomeSecteions/BigSection/BigSection.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const BigSection = () => {
+    return (
+        <div>
+            big section
+        </div>
+    );
+};
+
+export default BigSection;
---
+++ src/components/HomeSecteions/HeroSection.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import useSomething from '@/hooks/useSomething';
+
+const HeroSection = () => {
+    return (
+        <div>
+            hero
+        </div>
+    );
+};
+
+export default HeroSection;
